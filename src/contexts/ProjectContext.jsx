1eec296c589a7259ca60a6687bb5efdbb38024e9
allmygod <alleoin37@gmail.com>
2025-01-07 11:42:19 +0900

    feat: mock api & handle error

--- src/contexts/ProjectContext.jsx
+++ src/contexts/ProjectContext.jsx
@@ -1,57 +1,32 @@
-import React, { createContext, useState } from "react";
-import dayjs from "dayjs";
+import React, { createContext, useEffect, useState } from "react";
+import { fetchProjects } from "../lib/mockApi.js";
 
 export const ProjectContext = createContext();
 
 export const ProjectProvider = ({ children }) => {
-  const [data, setData] = useState([
-    {
-      key: "1",
-      projectId: "project_a",
-      projectName: "Project A",
-      startDate: dayjs("2025-01-01"),
-      endDate: dayjs("2025-12-31"),
-      projectManager: "John Doe",
-    },
-    {
-      key: "2",
-      projectId: "project_b",
-      projectName: "Project B",
-      startDate: dayjs("2025-01-01"),
-      endDate: dayjs("2025-12-31"),
-      projectManager: "John Doe",
-    },
-    {
-      key: "3",
-      projectId: "project_c",
-      projectName: "Project C",
-      startDate: dayjs("2025-01-01"),
-      endDate: dayjs("2025-12-31"),
-      projectManager: "John Doe",
-    },
-    {
-      key: "4",
-      projectId: "project_d",
-      projectName: "Project D",
-      startDate: dayjs("2025-01-01"),
-      endDate: dayjs("2025-12-31"),
-      projectManager: "John Doe",
-    },
-    {
-      key: "5",
-      projectId: "project_e",
-      projectName: "Project E",
-      startDate: dayjs("2025-01-01"),
-      endDate: dayjs("2025-12-31"),
-      projectManager: "John Doe",
-    },
-  ]);
+  const [data, setData] = useState([]);
   const [selected, setSelected] = useState(false);
   const [favorites, setFavorites] = useState(["project_a", "project_c"]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchProjects()
+      .then((initialData) => setData(initialData))
+      .catch((err) => setError(err));
+  }, []);
 
   return (
     <ProjectContext.Provider
-      value={{ data, setData, selected, setSelected, favorites, setFavorites }}
+      value={{
+        data,
+        setData,
+        selected,
+        setSelected,
+        favorites,
+        setFavorites,
+        error,
+        setError,
+      }}
     >
       {children}
     </ProjectContext.Provider>
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -6,19 +6,26 @@ import List from "../components/List.jsx";
 import Detail from "../components/Detail.jsx";
 import Favorites from "../components/Favorites.jsx";
 
+const ErrorMsg = styled.h2`
+  text-align: center;
+`;
+
 const CustomCol = styled(Col)`
   margin-top: 4rem;
   justify-content: center;
 `;
 
 export default function View() {
-  const { selected } = useContext(ProjectContext);
+  const { selected, error } = useContext(ProjectContext);
   return (
-    <Row>
-      <CustomCol span={4}>
-        <Favorites />
-      </CustomCol>
-      <CustomCol span={20}>{selected ? <Detail /> : <List />}</CustomCol>
-    </Row>
+    <>
+      {error && <ErrorMsg>{error}</ErrorMsg>}
+      <Row>
+        <CustomCol span={4}>
+          <Favorites />
+        </CustomCol>
+        <CustomCol span={20}>{selected ? <Detail /> : <List />}</CustomCol>
+      </Row>
+    </>
   );
 }
