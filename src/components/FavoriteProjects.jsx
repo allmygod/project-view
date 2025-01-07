1d7d05b64464d675af92cba971a81ca3c355e2ca
allmygod <alleoin37@gmail.com>
2025-01-07 10:45:08 +0900

    feat: add favorite project section

---
+++ src/components/FavoriteProjects.jsx
@@ -0,0 +1,29 @@
+import React, { useContext, useMemo } from "react";
+import styled from "@emotion/styled";
+import { ProjectContext } from "../contexts/ProjectContext.jsx";
+
+const HeaderText = styled.h3`
+  margin-left: 0.5rem;
+`;
+
+export default function FavoriteProjects() {
+  const { data, favorites } = useContext(ProjectContext);
+  const projects = useMemo(
+    () =>
+      data
+        .filter(({ projectId }) => favorites.includes(projectId))
+        .map(({ projectName }) => projectName),
+    [data]
+  );
+
+  return (
+    <>
+      <HeaderText>Favorite Projects</HeaderText>
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
--- src/pages/ProjectView.jsx
+++ src/pages/ProjectView.jsx
@@ -1,9 +1,26 @@
 import React, { useContext } from "react";
+import { Row, Col } from "antd";
+import styled from "@emotion/styled";
 import { ProjectContext } from "../contexts/ProjectContext.jsx";
 import ProjectTable from "../components/ProjectTable.jsx";
 import ProjectDetail from "../components/ProjectDetail.jsx";
+import FavoriteProjects from "../components/FavoriteProjects.jsx";
+
+const CustomCol = styled(Col)`
+  margin-top: 4rem;
+  justify-content: center;
+`;
 
 export default function ProjectView() {
   const { selected } = useContext(ProjectContext);
-  return selected ? <ProjectDetail /> : <ProjectTable />;
+  return (
+    <Row>
+      <CustomCol span={4}>
+        <FavoriteProjects />
+      </CustomCol>
+      <CustomCol span={20}>
+        {selected ? <ProjectDetail /> : <ProjectTable />}
+      </CustomCol>
+    </Row>
+  );
 }
